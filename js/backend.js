91499dd91a209cb64b944ee403d850749a5c62f8
Franck Kosellek <Franck Kosellek@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-03-08 18:37:48 +0000

    first commit
    git-svn-id: https://plugins.svn.wordpress.org/nwp-slideshow/trunk@516391 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ js/backend.js
@@ -0,0 +1,58 @@
+function resetForm() {
+    if (confirm(jQuery("#jsmess-reset").html()) == true)
+	jQuery("#resetForm").submit();
+}
+
+function deleteSlide() {
+    if (confirm(jQuery("#jsmess-delete").html()) == true)
+	jQuery("#deleteSlideForm").submit();
+}
+
+function osc(img, selection) {
+    jQuery("#textposy1").val(selection.y1);
+    jQuery("#textposy2").val(selection.y2);
+    jQuery("#textposx1").val(selection.x1);
+    jQuery("#textposx2").val(selection.x2);
+}
+
+function addSlide() {
+    if (jQuery("#addSlide").css("display") == "none")
+	jQuery("#addSlide").slideDown();
+    else
+	jQuery("#addSlide").slideUp();
+	
+}
+
+function advancedOptions() {
+    if (jQuery("#advancedOptions").css("display") == "none")
+	jQuery("#advancedOptions").slideDown();
+    else
+	jQuery("#advancedOptions").slideUp();
+}
+
+jQuery(document).ready(function() {
+
+    if (jQuery("#resetForm").length > 0) {
+	jQuery("#reset").click(function() { resetForm(); });
+	jQuery("#advancedButton").click(function() { advancedOptions(); });
+    }
+    else if (jQuery("#slideLst").length > 0) {
+	if (jQuery("#editSlide").length > 0) {
+	    var selection = jQuery('#slideImgFrame').imgAreaSelect({
+		handles: true,
+		x1:jQuery("#textposx1").val(),
+		y1:jQuery("#textposy1").val(),
+		x2:jQuery("#textposx2").val(),
+		y2:jQuery("#textposy2").val(),
+		persistent:true,
+		onSelectEnd: osc
+	    });
+	    jQuery(".delete").click(function() { deleteSlide(); });
+	}
+	if (jQuery("#addSlideButton").length > 0) {
+	    jQuery("#addSlideButton").click(function() { addSlide(); });
+	    jQuery("#cancel").click(function() { addSlide(); });
+	}
+    }
+
+});
---
+++ slideshow-frontend.php
@@ -0,0 +1,119 @@
+<?php
+
+
+
+
+function getSlides_last($nwpSlideshow) {
+  $args = Array(
+		"numberposts" => $nwpSlideshow['postsnbr'],
+		"orderby" => "post_date",
+		"order" => "DESC",
+		"post_status" => "publish",
+		"post_type" => "post"
+		);
+  if ($nwpSlideshow['type'] == "category") {
+    $args['category'] = $nwpSlideshow['category'];
+  }
+  $posts = get_posts($args);
+
+  /* print_r($posts); */
+  for ($i = 0; isset($posts[$i]); $i++) {
+    $text = "<div class='lastPostText'>".$posts[$i]->post_title."</div>";
+    $res[$i]['url'] = $posts[$i]->guid;
+    if( has_post_thumbnail($posts[$i]->ID) ) {
+      $image = wp_get_attachment_image_src( get_post_thumbnail_id( $posts[$i]->ID ), 'single-post-thumbnail' );
+      $res[$i]['image'] = $image[0];      
+    } else
+      $res[$i]['image'] = plugins_url("/images/noimage.png", __FILE__);
+    $res[$i]['text'] = $text;
+    $res[$i]['textposx1'] = 0;
+    $res[$i]['textposx2'] = $nwpSlideshow['sizewidth'];
+    $res[$i]['textposy1'] = $nwpSlideshow['sizeheight']-50;
+    $res[$i]['textposy2'] = 0;
+  }
+
+  /* print_r($posts); */
+
+  return $res;
+}
+
+function getSlides_category($nwpSlideshow) {
+  return getSlides_last($nwpSlideshow);
+}
+
+function getSlides_custom($nwpSlideshow) {
+  global $wpdb;
+  $q = "SELECT * FROM nwp_slideshow ORDER BY position ASC";
+  $slides = $wpdb->get_results($q);
+
+  $uri = explode("&", $_SERVER['REQUEST_URI']);
+  for ($i = 0; isset($slides[$i]); $i++) {
+    $res[$i]['url'] = $slides[$i]->url;
+    $res[$i]['image'] = plugins_url("/images/slides/".$slides[$i]->image, __FILE__);
+    $res[$i]['text'] = $slides[$i]->text;
+    $res[$i]['textposx1'] = $slides[$i]->textposx1;
+    $res[$i]['textposx2'] = $slides[$i]->textposx2;
+    $res[$i]['textposy1'] = $slides[$i]->textposy1;
+    $res[$i]['textposy2'] = $slides[$i]->textposy2;
+  }
+  return $res;
+}
+
+function displaySlideshow() {
+  global $nwpSlideshow;
+
+  /* What is the type of the Slideshow ? */
+  $getSlides = "getSlides_".$nwpSlideshow['type'];
+  $slides = $getSlides($nwpSlideshow);
+
+  /* print_r($slides); */
+
+
+
+  $s = '';
+
+  $s .= '<div id="nwpsWindow">';
+
+  $float = 0;
+  for ($i = 0; isset($slides[$i]); $i++) {
+    $width = $slides[$i]['textposx2'] - $slides[$i]['textposx1'];
+    $height = $slides[$i]['textposy2'] - $slides[$i]['textposy1'];
+    $top = $slides[$i]['textposy1'];
+    $left = $slides[$i]['textposx1'];
+    /* $style = "left:".$float."px; padding-left:".$left."px;  top:".$top."px; width:".$width."px; height:".$height."px;"; */
+    $style = "left:".$left."px;  top:".$top."px; width:".$width."px; height:".$height."px;";
+    if (!empty($slides[$i]['url']))
+      $s .= '<a href="'.$slides[$i]['url'].'" ';
+    else
+      $s .= '<div ';
+    $s .= 'class="nwpsSlide" id="nwpsSlide-'.($i+1).'" style="left:'.$float.'px;" >';
+    $s .= '<img id="nwpsImg-'.($i+1).'" class="nwpsImg" src="'.$slides[$i]['image'].'" alt="slide image"  >';
+    $s .= '<div id="nwpsText-'.($i+1).'" class="nwpsText" style="'.$style.' ">'.str_replace("\\", "", $slides[$i]['text']).'</div>'; 
+    if (!empty($slides[$i]['url']))
+      $s .= '</a>';
+    else
+      $s .= '</div>';
+    $float += $nwpSlideshow['sizewidth'];
+  }
+
+
+  /* Controls */
+  $s .= '<p id="nwpsControls" style="left:'.$nwpSlideshow['buttonposx'].'px; top:'.$nwpSlideshow['buttonposy'].'px;">';
+  for ($i = 0; isset($slides[$i]); $i++) {
+    $s .= '<img src="'.plugins_url("/images/slides/offbutton.png", __FILE__).'" class="nwpsNav" id="nwpsNav-'.($i+1).'" />';
+  }
+  $s .= '</p>';
+
+  $s .= '</div>'; // #nwpsWindow
+
+
+
+  return $s;
+  
+}
+
+
+
+
+
+?>
